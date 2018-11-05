17a4db968d0a1ffbe7eed8ee9f6d26e4cf5d85f3
JSilvas <jaywsilvas@gmail.com>
2018-11-04 23:59:11 -0800

    Updated npm merge dependency

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MasonryGallery from './components/masonry-gallery.js';
 
 class App extends Component {
+  
+  //  ES6 method of initializing
+  // state = {
+
+  // }
+  
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+    this.handleCount = this.handleCount.bind(this);
+  }
+
+  handleCount() {
+    this.setState({ 
+      counter: this.state.counter + 1,
+      color: 'red'
+    })
+  }
+
   testFunction() {
     console.log('I function!');
   }
@@ -16,7 +36,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={this.testFunction} >Click me!</button>
+        <button onClick={this.testFunction}>Click me!</button>
+        <button onClick={this.handleCount}>Count me!</button>
+        <span>{this.state.counter}</span>
+        <MasonryGallery />
       </div>
     );
   }
